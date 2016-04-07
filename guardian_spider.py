a4ae257591a2877d2b8f2458cc35d92647bac5a4
VINEEL KONERU <vineelkoneru@VINEELs-MacBook-Air.local>
2016-04-07 15:52:19 +1000

    inserting data into mongo collection

---
+++ guardian_spider.py
@@ -0,0 +1,33 @@
+import scrapy
+from mongo import mongoFront
+
+
+handle = mongoFront()
+
+class StackOverflowSpider(scrapy.Spider):
+    name = 'guardian'
+    start_urls = ["http://www.theguardian.com/news/2016/apr/05/all" ]
+
+    def parse(self, response):
+        count = 0
+        for href in response.css('.fc-container__inner .fc-slice-wrapper .fc-item__container .fc-item__content a::attr(href)'):
+            full_url = response.urljoin(href.extract())
+            print full_url
+            count = count + 1
+            yield scrapy.Request(full_url, callback=self.parse_each_link)
+        print "totally " + str(count) + " links"
+        # yield scrapy.Request("http://www.theguardian.com/news/commentisfree/2016/apr/05/panama-papers-britain-house-order-cameron", callback=self.parse_each_link)
+
+    def parse_each_link(self, response):
+        print response
+        datatowrite = {}
+        datatowrite['title'] = response.css('.content__headline::text ').extract()[0]
+        datatowrite['minExplanation'] = response.css('.content__standfirst::text').extract()[1]
+        datatowrite['points'] = []
+        for main_links in response.css('.content__standfirst a::text'):
+            datatowrite['points'].append(main_links.extract())
+
+        datatowrite['body'] = []
+        for each_para in  response.css('.content__article-body p::text'):
+            datatowrite['body'].append(each_para.extract())
+        handle.upload(datatowrite)
--- mongo.py
+++ mongo.py
@@ -4,33 +4,13 @@ import ssl
 
 
 
-class mongoFront:
-    MONGODB_URL = "mongodb://tester:tester@aws-us-east-1-portal.13.dblayer.com:11066/articles"
-    def get_connection(self):
+class mongoFront:                         
+    MONGODB_URL = "mongodb://tester:tester@aws-us-east-1-portal.12.dblayer.com:10652/articles"
+    def __init__(self):
         client = pymongo.MongoClient(mongoFront.MONGODB_URL,ssl_cert_reqs=ssl.CERT_NONE)
-        db = client.get_default_database()
-        print db.collection_names()
-        return db
+        self.db = client.get_default_database()
 
     def upload(self,data):
-        print "connection"
-        print data
-        connection = self.get_connection()
-        print "test"
-
-
-obj = mongoFront()
-print "test"
-dbhandle = obj.get_connection()
-dbhandle.collection_names()
-
-
-
-
-# collection = dbhandle.test
-
-
-# MONGODB_URL = "mongodb://tester:tester@aws-us-east-1-portal.13.dblayer.com:11066/articles"
-# client = pymongo.MongoClient(MONGODB_URL,ssl_cert_reqs=ssl.CERT_NONE)
-# db = client.get_default_database()
-# print db.collection_names()
+        print "uploading data to mongodb "
+        # print self.db.collection_names()
+        print self.db.test.insert_one(data)
